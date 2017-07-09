d63a178c552a54b9efa6aa4bee8eadecf7a09edd
Hugh Willson <hugh@octonary.com>
2017-07-08 20:37:05 -0400

    Package adjustments.

--- packages/ejson/package.js
+++ packages/ejson/package.js
@@ -4,17 +4,16 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-  api.use(['underscore', 'base64']);
+  api.use(['ecmascript', 'underscore', 'base64']);
   api.export('EJSON');
-  api.export('EJSONTest', {testOnly: true});
-  api.addFiles('ejson.js', ['client', 'server']);
-  api.addFiles('stringify.js', ['client', 'server']);
+  api.export('EJSONTest', { testOnly: true });
+  api.addFiles('stringify.js');
+  api.mainModule('ejson.js');
 });
 
 Package.onTest(function (api) {
   api.use('ejson', ['client', 'server']);
-  api.use(['tinytest', 'underscore']);
-
-  api.addFiles('custom_models_for_tests.js', ['client', 'server']);
-  api.addFiles('ejson_test.js', ['client', 'server']);
+  api.use(['ecmascript', 'tinytest', 'underscore']);
+  api.addFiles('custom_models_for_tests.js');
+  api.addFiles('ejson_test.js');
 });
